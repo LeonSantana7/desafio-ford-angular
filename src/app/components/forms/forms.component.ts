484fc5b85638dc28df8de835bd4ae1019c0a4ae0
Leon Santana <leon.santana854@gmail.com>
2025-04-30 21:05:06 -0300

    feat:ajustes na tela de forms

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,16 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
-  
+
   forms = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
   });
+
+
+  salvar() {
+    this.forms.markAllAsTouched();
+
+    if (this.forms.valid) {
+      console.log("Valores digitados", this.forms.value);
+    }
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.forms.get(nomeCampo);
+    return campo?.invalid && campo?.touched && (campo?.errors?.['required'] || campo?.errors?.['email']) || false;
+  }
 }
